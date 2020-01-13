fb219e36413727961bf4476fc7a39d006dddb578
Laerton Marques de Figueiredo <laerton240311@gmail.com>
2020-01-12 22:23:04 -0300

    Adicionado o projeto de exemplo do uso da Frame SpecFlow

---
+++ Calculadora/Calc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    public static class Calc
+    {
+        /// <summary>
+        /// Soma dois inteiros
+        /// </summary>
+        /// <param name="numb1"> Primeiro inteiro</param>
+        /// <param name="numb2"> Segund inteiro</param>
+        /// <returns>Soma dos valores.</returns>
+        public static long SomaInteiros( long numb1, long numb2)
+        {
+            return numb1 + numb2;
+        }
+    }
+}
---
+++ NUnitTest1/SpecFlowFeature1Steps.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace NUnitTest1
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private int num;
+        private int sum;
+
+        [BeforeScenario]
+        public void Init()
+        {
+            num = 0;
+            sum = 0;
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            num = p0;
+        }
+
+     
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            sum += num;
+            num = 0;
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(p0, sum);
+        }
+    }
+}
